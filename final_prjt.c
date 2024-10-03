1e147fa367d785af39663231993c1005fc0cc171
Yasmineebh1 <bahiyasmine.13@gmail.com>
2024-10-03 17:24:13 +0200

    affichage

--- final_prjt.c
+++ final_prjt.c
@@ -14,19 +14,22 @@ char date[MAX][20];
 int count_rev = 0;
 
 // ajout des reservations
-void ajout() {
+void ajout()
+{
     int n;
     printf("Donnez-moi le nombre de reservations que vous voulez ajouter: ");
     scanf("%d", &n);
 
-    if (count_rev + n > MAX) {
+    if (count_rev + n > MAX)
+    {
         printf("Le maximum de reservations a atteint.\n");
         return;
     }
 
-    for (int i = 0; i < n; i++) {
-         printf("reference %d\n", i + 1);
-            // scanf("%d",&reservation.reference); 
+    for (int i = 0; i < n; i++)
+    {
+        printf("reference %d\n", reference[count_rev] = count_rev + 1);
+        // scanf("%d",&reservation.reference);
         printf("Ajouter le Nom: ");
         scanf("%s", nom[count_rev]);
         printf("Ajouter le Prenom: ");
@@ -40,64 +43,76 @@ void ajout() {
         printf("Ajouter la Date de reservation (DD-MM-YYYY): ");
         scanf("%s", date[count_rev]);
 
-        count_rev++;
         printf("Reservation ajoutee avec succes.\n");
+        count_rev++;
     }
 }
 
-//function pour modifier
-void choix_modification(int a){
+// function pour modifier
+void choix_modification()
+{
+    int ref;
+    int found = 0;
 
-    int choix;
-    do {
-        printf("1. Modifier le Nom\n");
-        printf("2. Modifier le Prenom\n");
-        printf("3. Modifier le Telephone\n");
-        printf("4. Modifier l'Age\n");
-        printf("5. Modifier le Status\n");
-        printf("6. Modifier la Date\n");
-        printf("7. Quitter la modification\n");
-        printf("Choisissez votre choix: ");
-        scanf("%d", &choix);
+    printf("Entrer la reference de la reservation: ");
+    scanf("%d", &ref);
+    for (int i = ref; i < count_rev; i++)
+    {
+        found = 1;
+        int choix;
+        do
+        {
+            printf("1. Modifier le Nom\n");
+            printf("2. Modifier le Prenom\n");
+            printf("3. Modifier le Telephone\n");
+            printf("4. Modifier l'Age\n");
+            printf("5. Modifier le Status\n");
+            printf("6. Modifier la Date\n");
+            printf("7. Quitter la modification\n");
+            printf("Choisissez votre choix: ");
+            scanf("%d", &choix);
 
-        switch (choix) {
+            switch (choix)
+            {
             case 1:
                 printf("donner le nouveau nom : ");
-                scanf("%s", nom[a]);
+                scanf("%s", nom[i]);
                 break;
             case 2:
                 printf("donner le nouveau prenom: ");
-                scanf("%s", prenom[a]);
+                scanf("%s", prenom[i]);
                 break;
             case 3:
                 printf("donner le nouveau telephone: ");
-                scanf("%s", telephone[a]);
+                scanf("%s", telephone[i]);
                 break;
             case 4:
                 printf("donner le Nouvel age: ");
-                scanf("%d", &age[a]);
+                scanf("%d", &age[i]);
                 break;
             case 5:
                 printf("donner le nouveau status: ");
-                scanf("%s", status[a]);
+                scanf("%s", status[i]);
                 break;
             case 6:
                 printf("donner la nouvelle date: ");
-                scanf("%s", date[a]);
+                scanf("%s", date[i]);
                 break;
             case 7:
                 printf("Quitter\n");
-                return ;
+                return;
                 break;
             default:
                 printf("Choix invalide.\n");
                 break;
-        }
-    } while (choix != 7);
+            }
+        } while (choix != 7);
+    }
 }
 
 // fonction de modification / suppression
-void modifier_supprimer() {
+void modifier_supprimer()
+{
     int ref;
     int choix;
 
@@ -107,46 +122,52 @@ void modifier_supprimer() {
     printf("Choisissez votre choix: ");
     scanf("%d", &choix);
 
-    printf("Entrer la reference de la reservation: ");
-    scanf("%d", &ref);
-
-    if (ref < 1 || ref > count_rev) {
+    if (ref < 1 || ref > count_rev)
+    {
         printf("ce reference n ya pas dans la liste\n");
         return;
     }
+    int found = 0;
+    switch (choix)
+    {
+    case 1:
 
-        switch (choix) {
-        case 1:
-        if(ref == reference){
-            choix_modification(ref);
-            break;}
-        case 2:
-            for (int i = ref; i < count_rev; i++) {
-                strcpy(nom[i], nom[i + 1]);
-                strcpy(prenom[i], prenom[i + 1]);
-                strcpy(telephone[i], telephone[i + 1]);
-                age[i] = age[i + 1];
-                strcpy(status[i], status[i + 1]);
-                strcpy(date[i], date[i + 1]);
-                reference[i] = reference[i + 1];
-            }
-            count_rev--;
-            printf("Reservation supprimee .\n");
-            break;
-        default:
-            printf("Choix .\n");
-            break;
+        choix_modification(ref);
+        break;
+
+    case 2:
+        printf("Entrer la reference de la reservation: ");
+        scanf("%d", &ref);
+        for (int i = ref; i < count_rev; i++)
+        {
+            strcpy(nom[i], nom[i + 1]);
+            strcpy(prenom[i], prenom[i + 1]);
+            strcpy(telephone[i], telephone[i + 1]);
+            age[i] = age[i + 1];
+            strcpy(status[i], status[i + 1]);
+            strcpy(date[i], date[i + 1]);
+            reference[i] = reference[i + 1];
+        }
+        count_rev--;
+        printf("Reservation supprimee .\n");
+        break;
+    default:
+        printf("\n");
+        break;
     }
 }
 
 // Function to display reservations
-void afficher() {
-    if (count_rev == 0) {
+void afficher()
+{
+    if (count_rev == 0)
+    {
         printf("nya pas de reservation.\n");
         return;
     }
 
-    for (int i = 1; i < count_rev; i++) {
+    for (int i = 0; i < count_rev; i++)
+    {
         printf("Reference: %d\n", reference[i]);
         printf("Nom: %s\n", nom[i]);
         printf("Prenom: %s\n", prenom[i]);
@@ -156,11 +177,17 @@ void afficher() {
         printf("Date: %s\n", date[i]);
         printf("\n");
     }
+    count_rev++;
 }
-
-int main() {
+//function de recherche
+void rechercher()
+{
+}
+int main()
+{
     int choix;
-    do {
+    do
+    {
         printf("1. Ajouter une reservation.\n");
         printf("2. Modifier ou supprimer une reservation.\n");
         printf("3. Afficher les details d une reservation.\n");
@@ -170,23 +197,24 @@ int main() {
         printf("7. Quitter.\n");
         scanf("%d", &choix);
 
-        switch (choix) {
-            case 1:
-                ajout();
-                break;
-            case 2:
-                modifier_supprimer();
-                break;
-            case 3:
-                afficher();
-                break;
-            case 7:
-               // printf("quitter!\n");
-                return;
-                break;
-            default:
-                printf(" invalide.\n");
-                break;
+        switch (choix)
+        {
+        case 1:
+            ajout();
+            break;
+        case 2:
+            modifier_supprimer();
+            break;
+        case 3:
+            afficher();
+            break;
+        case 7:
+            printf("quitter!\n");
+            return 0;
+            break;
+        default:
+            printf(" invalide.\n");
+            break;
         }
     } while (choix != 7);
 
