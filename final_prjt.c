469690ea074d093fd28cc7d39c4ddb6dbd04d18a
Yasmineebh1 <bahiyasmine.13@gmail.com>
2024-10-03 12:30:18 +0200

    updating ajouter

--- final_prjt.c
+++ final_prjt.c
@@ -18,35 +18,44 @@ struct Reservation
 // declaration globale
 int count_rev = 0;
 int i = 0;
-
-
+int n;
+struct Reservation reservation;
 // ajout
 void ajout()
 {
 
     if (count_rev < MAX)
     {
+        printf("donne moi le nombre de reservation que vous voules ajouter: ");
+    scanf("%d",&n);
 
-        struct Reservation reservation;
-        printf("reference %d\n", i + 1);
-        // scanf("%d",&reservation.reference);
-        printf("Ajouter le Nom: ");
-        scanf("%s", &reservation.Nom[i]);
-        printf("Ajouter le Prenom: ");
-        scanf("%s", &reservation.prenom[i]);
-        printf("Ajouter le Telephone (xxxx-xx-xxxx): ");
-        scanf("%s", &reservation.telephone);
-        printf("Ajouter Age: ");
-        scanf("%d", &reservation.age);
-        printf("Ajouter le Status (valide, reporte, annule, traite): ");
-        scanf("%s", &reservation.status[i]);
-        printf("Ajouter la Date de reservation(DD-MM-YYYY): ");
-        scanf("%s", &reservation.date);
+    for (int i = count_rev; i < n; i++)
+        {
 
-        printf("reservation ajoutee avec succees\n");
-        // ajout la nouvel reserrvation
-        
-        count_rev++;
+            //reservation.reference[i] = n + 1;
+            printf("reference %d\n", i + 1);
+           // scanf("%d",&reservation.reference);
+            printf("Ajouter le Nom: ");
+            scanf("%s", reservation.Nom[i]);
+            printf("Ajouter le Prenom: ");
+            scanf("%s", reservation.prenom[i]);
+            printf("Ajouter le Telephone (xxxx-xx-xxxx): ");
+            scanf("%s", reservation.telephone);
+            printf("Ajouter Age: ");
+            scanf("%d", &reservation.age);
+            printf("Ajouter le Statut (valide, reporte, annule, traite): ");
+            scanf("%s", reservation.status[i]);
+            //status(reservation.status);
+            printf("Ajouter la Date de reservation(DD-MM-YYYY): \n");
+            scanf("%s", reservation.date);
+
+            printf("reservation ajoutee avec succees\n");
+            // ajout la nouvel reserrvation
+            reservation.reference = count_rev;
+
+        }
+         count_rev+=n;
+    
     }
 
     // gonna show when the MAX > 100
@@ -54,8 +63,48 @@ void ajout()
         printf("le maximum de reservation a ete atteint.\n");
 }
 
+/*
 void modifier_supprimer()
 {
+    int ref;
+    int choix;
+    int found = 0;
+    struct Reservation reservation;
+    printf("Modifier ou supprimer une réservation existante en fonction de la référence unique.");
+    printf("1.Modifier la reservation");
+    printf("2.supprimer la reservation");
+    switch (choix)
+    {
+        // modification
+    case 1:
+        printf("entrer le reference de patient que voulez vous le modifier: ");
+        scanf("%d", &ref);
+
+        for (int i = 0; i < count_rev; i++)
+        {
+            if (strcmp(ref,reservation.reference))
+            {
+                found = 1;
+                
+            }
+            
+            count_rev++;
+        }
+        break;
+        // suppression
+    case 2:
+        printf("entrer le reference de patient que voulez vous le modifier: ");
+        scanf("%d", &ref);
+
+        break;
+    default:
+        break;
+    }
+}*/
+
+void afficher()
+{
+    printf("");
 }
 // fake ddata
 //  struct ten_fake_data(){
@@ -89,8 +138,10 @@ int main()
         case 1:
             ajout();
             break;
-
-        default:
+        // case 2:
+        //     modifier_supprimer();
+        //     break;
+        // default:
             break;
         }
     } while (choix != 7);
